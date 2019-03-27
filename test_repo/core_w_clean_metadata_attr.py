feee7f35938ebb732a5325beaef9814f13a58498
Arthur Goldberg <artgoldberg@gmail.com>
2019-03-27 17:43:10 -0400

    create additional test data

---
+++ test_repo/core_w_clean_metadata_attr.py
@@ -0,0 +1,30 @@
+""" Example schema for testing migration
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2019-2-12
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+
+from obj_model import SlugAttribute, StringAttribute, ManyToManyAttribute, TabularOrientation
+import obj_model
+
+
+class Test(obj_model.Model):
+    id = SlugAttribute()
+    name = StringAttribute(default='test')
+    url = StringAttribute()
+    branch = StringAttribute()
+    revision = StringAttribute()
+    existing_attr = StringAttribute(default='existing_attr_val')
+    # comment on master branch
+
+    class Meta(obj_model.Model.Meta):
+        attribute_order = ('id', 'name', 'revision', 'existing_attr')
+        tabular_orientation = TabularOrientation.column
+_GIT_METADATA = (Test, ('url', 'branch', 'revision'))
+
+
+class Foo(obj_model.Model):
+    id = SlugAttribute()
