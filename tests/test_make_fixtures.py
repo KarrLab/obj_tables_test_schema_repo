4562d73e38ec8dbc48dba185f877081fb8345651
Arthur Goldberg <artgoldberg@gmail.com>
2019-06-17 16:12:35 -0400

    remove unneeded file & code

--- tests/test_make_fixtures.py
+++ tests/test_make_fixtures.py
@@ -36,29 +36,3 @@ class TestMakeFixtureFiles(unittest.TestCase):
         fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
         fixture_file = os.path.join(fixtures_path, 'bad_data_file.xlsx')
         obj_model.io.Writer().run(fixture_file, [test_new_1, test_new_2], models=[TestNew, Reference])
-
-
-from test_repo.core_w_clean_metadata_attr import Test, Foo
-@unittest.skip("not a test; use when fixture(s) need updating")
-class TestMakeFixtureFileUsingCleanMetadata(unittest.TestCase):
-
-    def test_make_fixtures(self):
-
-        test_new_1 = Test(
-            id='test_new_1',
-            existing_attr='x',
-        )
-        test_new_2 = Test(
-            id='test_new_2',
-            existing_attr='x',
-        )
-        set_git_repo_metadata_from_path(test_new_1)
-        set_git_repo_metadata_from_path(test_new_2)
-
-        foo = Foo(
-            id='id_1'
-        )
-
-        fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
-        fixture_file = os.path.join(fixtures_path, 'bad_data_file_2.xlsx')
-        obj_model.io.Writer().run(fixture_file, [test_new_1, test_new_2, foo], models=[Test, Foo])
