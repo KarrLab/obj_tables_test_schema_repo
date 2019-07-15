00201c04576f5113074b6fe3077f851b4cd9af17
Arthur Goldberg <artgoldberg@gmail.com>
2019-07-15 13:21:07 -0400

    add another example error

---
+++ migrations/no_transformations_wrapper.py
@@ -0,0 +1,13 @@
+from obj_model.migrate import MigrationWrapper
+
+
+class NullWrapper(MigrationWrapper):
+
+    # make a wrapper with prepare_existing_models & modify_migrated_models that do nothing
+    def prepare_existing_models(self, migrator, existing_models):
+        pass
+
+    def modify_migrated_models(self, migrator, migrated_models):
+        pass
+
+not_transformations = NullWrapper()
